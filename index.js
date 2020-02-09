5b2d93c64a3ed60f8a4b556746034fc5f46bdc62
Robert Anthonie Soriano <sorianorobertc@gmail.com>
2020-02-09 21:47:10 +0800

    [fix][wip] Fix incorrect recovered and death values

--- index.js
+++ index.js
@@ -1,52 +1,52 @@
-const express = require('express')
-const cors = require('cors')
-const morgan = require('morgan')
-const fs = require('fs')
-const path = require('path')
-const apicache = require('apicache')
-const compression = require('compression')
-const Scraper = require('./services/Scraper')
-const Twitter = require('./services/Twitter')
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const fs = require('fs');
+const path = require('path');
+const apicache = require('apicache');
+const compression = require('compression');
+const Scraper = require('./services/Scraper');
+const Twitter = require('./services/Twitter');
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
-const app = express()
-const cache = apicache.middleware
+const app = express();
+const cache = apicache.middleware;
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN
   })
-)
-app.use(compression())
+);
+app.use(compression());
 // create a write stream (in append mode)
 const accessLogStream = fs.createWriteStream(
   path.join(__dirname, 'access.log'),
   { flags: 'a' }
-)
+);
 
 // setup the logger
-app.use(morgan('combined', { stream: accessLogStream }))
+app.use(morgan('combined', { stream: accessLogStream }));
 
-app.get('/api/cases', cache('1 hour'), async (req, res) => {
-  const scraper = new Scraper()
-  const data = await scraper.fetchTimeSeries()
-  return res.json(data)
-})
+app.get('/api/cases', async (req, res) => {
+  const scraper = new Scraper();
+  const data = await scraper.fetchTimeSeries();
+  return res.json(data);
+});
 
 app.get('/api/tweets', cache('5 minutes'), async (req, res) => {
-  const twit = new Twitter()
-  const data = await twit.getTweets(req.query)
-  return res.json(data)
-})
+  const twit = new Twitter();
+  const data = await twit.getTweets(req.query);
+  return res.json(data);
+});
 
 app.get('/api/timeline', cache('5 hours'), async (req, res) => {
-  const scraper = new Scraper()
-  const data = await scraper.getTimeline()
-  return res.json(data)
-})
+  const scraper = new Scraper();
+  const data = await scraper.getTimeline();
+  return res.json(data);
+});
 
-app.get('*', (req, res) => res.send('Page Not found'))
+app.get('*', (req, res) => res.send('Page Not found'));
 
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`)
-})
+  console.log(`App listening on port ${PORT}!`);
+});
--- services/Scraper.js
+++ services/Scraper.js
@@ -2,6 +2,7 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const dotenv = require('dotenv');
 const { GoogleSpreadsheet } = require('google-spreadsheet');
+const moment = require('moment');
 dotenv.config();
 
 // Novel Coronavirus (2019-nCoV) Cases, provided by JHU CSSE
@@ -11,169 +12,187 @@ const doc = new GoogleSpreadsheet(sheetId);
 doc.useApiKey(process.env.GOOGLE_API_KEY);
 
 class Scraper {
-	async getHTML(url) {
-		const res = await axios(url);
-		return cheerio.load(res.data);
-	}
+  async getHTML(url) {
+    const res = await axios(url);
+    return cheerio.load(res.data);
+  }
 
-	async getTimeline() {
-		const $ = await this.getHTML(
-			'https://bnonews.com/index.php/2020/01/timeline-coronavirus-epidemic/'
-		);
-		const timelineDiv = $('#mvp-content-main');
+  async getTimeline() {
+    const $ = await this.getHTML(
+      'https://bnonews.com/index.php/2020/01/timeline-coronavirus-epidemic/'
+    );
+    const timelineDiv = $('#mvp-content-main');
 
-		const data = timelineDiv
-			.find('h4')
-			.toArray()
-			.map((h4, h4Idx) => ({
-				date: $(h4)
-					.text()
-					.trim(),
-				time: timelineDiv
-					.find('ul')
-					.eq(h4Idx)
-					.children('li')
-					.toArray()
-					.map(li => ({
-						time_and_description: $(li)
-							.text()
-							.trim()
-							.replace(' (Source)', ''),
-						source: $(li)
-							.find('a')
-							.attr('href')
-					}))
-			}));
-		const latest = await this.getTimelineLatest();
-		return [...latest, ...data].map(item => {
-			return {
-				...item,
-				time: item.time
-					.filter(
-						i => !i.time_and_description.includes('Total at the end of the day')
-					)
-					.map(i => ({
-						time: i.time_and_description.slice(0, 5),
-						description: i.time_and_description.replace(
-							`${i.time_and_description.slice(0, 5)}: `,
-							''
-						),
-						source: i.source
-					}))
-			};
-		});
-	}
+    const data = timelineDiv
+      .find('h4')
+      .toArray()
+      .map((h4, h4Idx) => ({
+        date: $(h4)
+          .text()
+          .trim(),
+        time: timelineDiv
+          .find('ul')
+          .eq(h4Idx)
+          .children('li')
+          .toArray()
+          .map(li => ({
+            time_and_description: $(li)
+              .text()
+              .trim()
+              .replace(' (Source)', ''),
+            source: $(li)
+              .find('a')
+              .attr('href')
+          }))
+      }));
+    const latest = await this.getTimelineLatest();
+    return [...latest, ...data].map(item => {
+      return {
+        ...item,
+        time: item.time
+          .filter(
+            i => !i.time_and_description.includes('Total at the end of the day')
+          )
+          .map(i => ({
+            time: i.time_and_description.slice(0, 5),
+            description: i.time_and_description.replace(
+              `${i.time_and_description.slice(0, 5)}: `,
+              ''
+            ),
+            source: i.source
+          }))
+      };
+    });
+  }
 
-	async getTimelineLatest() {
-		const $ = await this.getHTML(
-			'https://bnonews.com/index.php/2020/02/the-latest-coronavirus-cases/'
-		);
-		const data = [];
-		$('h2:contains("Timeline (GMT)")')
-			.nextUntil('h3')
-			.each((idx, el) => {
-				if (el.name === 'h4') {
-					const obj = {
-						date: $(el)
-							.text()
-							.trim(),
-						time: $(el)
-							.next()
-							.children('li')
-							.toArray()
-							.map(li => ({
-								time_and_description: $(li)
-									.text()
-									.trim()
-									.replace(' (Source)', ''),
-								source: $(li)
-									.find('a')
-									.attr('href')
-							}))
-					};
+  async getTimelineLatest() {
+    const $ = await this.getHTML(
+      'https://bnonews.com/index.php/2020/02/the-latest-coronavirus-cases/'
+    );
+    const data = [];
+    $('h2:contains("Timeline (GMT)")')
+      .nextUntil('h3')
+      .each((idx, el) => {
+        if (el.name === 'h4') {
+          const obj = {
+            date: $(el)
+              .text()
+              .trim(),
+            time: $(el)
+              .next()
+              .children('li')
+              .toArray()
+              .map(li => ({
+                time_and_description: $(li)
+                  .text()
+                  .trim()
+                  .replace(' (Source)', ''),
+                source: $(li)
+                  .find('a')
+                  .attr('href')
+              }))
+          };
 
-					data.push(obj);
-				}
-			});
+          data.push(obj);
+        }
+      });
 
-		return data;
-	}
+    return data;
+  }
 
-	async fetchTimeSeries() {
-		try {
-			await doc.loadInfo();
+  async fetchTimeSeries() {
+    const formatDate = date => moment(new Date(date)).format('M/D/YYYY');
 
-			// Load headers
-			const sheet = doc.sheetsByIndex[0];
-			await sheet.loadHeaderRow();
-			const headers = [...sheet.headerValues.slice(0, 5)];
-			sheet.headerValues.slice(5).forEach(item => {
-				const date = item.split(' ')[0];
-				const idx = headers.map(i => i.split(' ')[0]).indexOf(date);
-				if (idx === -1) {
-					headers.push(item);
-				} else {
-					headers[idx] = item;
-				}
-			});
+    try {
+      await doc.loadInfo();
 
-			const data = [];
-			// Load confirmed sheets
-			const confirmedSheet = doc.sheetsByIndex[0];
-			const confirmedSheetRows = await confirmedSheet.getRows();
+      const data = [];
 
-			// Load recovered sheet
-			const recoveredSheet = doc.sheetsByIndex[1];
-			const recoveredSheetRows = await recoveredSheet.getRows();
+      // Load confirmed sheets
+      const confirmedSheet = doc.sheetsByIndex[0];
+      const confirmedSheetRows = await confirmedSheet.getRows();
+      await confirmedSheet.loadHeaderRow();
 
-			// Load death sheet
-			const deathSheet = doc.sheetsByIndex[2];
-			const deathSheetRows = await deathSheet.getRows();
+      const headers = [...confirmedSheet.headerValues.slice(0, 5)];
+      confirmedSheet.headerValues.slice(5).forEach(item => {
+        const idx = headers.map(i => formatDate(i)).indexOf(formatDate(item));
+        if (idx === -1) {
+          headers.push(formatDate(item));
+        } else {
+          headers[idx] = formatDate(item);
+        }
+      });
 
-			confirmedSheetRows.forEach(row => {
-				const obj = {};
-				headers.slice(0, 5).forEach(header => {
-					obj[header] = row[header];
-				});
-				obj.dates = [];
-				headers.slice(5).forEach(header => {
-					obj.dates.push({
-						date: header.split(' ')[0],
-						confirmed: +row[header] || 0,
-						recovered:
-							+recoveredSheetRows.find(
-								i => i.Lat.trim() === row.Lat.trim() && i.Long.trim() === row.Long.trim()
-							)[header.trim()] || 0,
-						death:
-						+deathSheetRows.find(
-							i => i.Lat.trim() === row.Lat.trim() && i.Long.trim() === row.Long.trim()
-						)[header.trim()] || 0,
-					});
-				});
+      // Load recovered sheet
+      const recoveredSheet = doc.sheetsByIndex[1];
+      const recoveredSheetRows = await recoveredSheet.getRows();
+      await recoveredSheet.loadHeaderRow();
 
-				data.push(obj);
-			});
+      // Load death sheet
+      const deathSheet = doc.sheetsByIndex[2];
+      const deathSheetRows = await deathSheet.getRows();
+      await deathSheet.loadHeaderRow();
 
-			return {
-				total_confirmed: data.reduce(
-					(a, b) => a + b.dates[b.dates.length - 1].confirmed,
-					0
-				),
-				total_recovered: data.reduce(
-					(a, b) => a + b.dates[b.dates.length - 1].recovered,
-					0
-				),
-				total_death: data.reduce(
-					(a, b) => a + b.dates[b.dates.length - 1].death,
-					0
-				),
-				last_update: new Date(),
-				data
-			};
-		} catch (e) {
-			throw e;
-		}
-	}
+      confirmedSheetRows.forEach(row => {
+        const obj = {};
+        headers.slice(0, 5).forEach(header => {
+          obj[header] = row[header];
+        });
+        obj.dates = [];
+        headers.slice(5).forEach(header => {
+          const confirmedSheetHeaders = confirmedSheet.headerValues.filter(
+            i => formatDate(i) === header
+          );
+          const recoveredSheetHeaders = recoveredSheet.headerValues.filter(
+            i => formatDate(i) === header
+          );
+          const deathSheetHeaders = deathSheet.headerValues.filter(
+            i => formatDate(i) === header
+          );
+
+          obj.dates.push({
+            date: header.split(' ')[0],
+            confirmed:
+              +row[confirmedSheetHeaders[confirmedSheetHeaders.length - 1]] ||
+              0,
+            recovered:
+              +recoveredSheetRows.find(
+                i =>
+                  i.Lat.trim() === row.Lat.trim() &&
+                  i.Long.trim() === row.Long.trim()
+              )[recoveredSheetHeaders[recoveredSheetHeaders.length - 1]] || 0,
+            death:
+              +deathSheetRows.find(
+                i =>
+                  i.Lat.trim() === row.Lat.trim() &&
+                  i.Long.trim() === row.Long.trim()
+              )[deathSheetHeaders[recoveredSheetHeaders.length - 1]] || 0
+          });
+        });
+
+        data.push(obj);
+      });
+
+      return {
+        total_confirmed: data.reduce(
+          (a, b) => a + b.dates[b.dates.length - 1].confirmed,
+          0
+        ),
+        total_recovered: data.reduce(
+          (a, b) => a + b.dates[b.dates.length - 1].recovered,
+          0
+        ),
+        total_death: data.reduce(
+          (a, b) => a + b.dates[b.dates.length - 1].death,
+          0
+        ),
+        last_update: new Date(),
+        data
+      };
+    } catch (e) {
+      throw e;
+    }
+  }
 }
 
 module.exports = Scraper;

c6f7737ae23fd56e3cec6511f93ee4de3838b0a6
Robert Anthonie Soriano <sorianorobertc@gmail.com>
2020-02-11 19:54:03 +0800

    [refactor] Remove google-spreadsheet and parse csv from new github link

--- services/Scraper.js
+++ services/Scraper.js
@@ -1,17 +1,15 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
-const dotenv = require('dotenv');
-const { GoogleSpreadsheet } = require('google-spreadsheet');
 const moment = require('moment');
-dotenv.config();
-
-// Novel Coronavirus (2019-nCoV) Cases, provided by JHU CSSE
-// Load google sheet
-const sheetId = '1UF2pSkFTURko2OvfHWWlFpDFAr1UxCBA4JLwlSP6KFo';
-const doc = new GoogleSpreadsheet(sheetId);
-doc.useApiKey(process.env.GOOGLE_API_KEY);
+const request = require('request');
+const csv = require('csvtojson');
 
 class Scraper {
+  constructor() {
+    this.timeSeriesURL =
+      'https://raw.githubusercontent.com/CSSEGISandData/2019-nCoV/master/time_series';
+  }
+
   async getHTML(url) {
     const res = await axios(url);
     return cheerio.load(res.data);
@@ -102,96 +100,118 @@ class Scraper {
 
   async fetchTimeSeries() {
     const formatDate = date => moment(new Date(date)).format('M/D/YYYY');
+    const roundOffCoord = coord => parseFloat(coord.trim()).toFixed(5);
 
-    try {
-      await doc.loadInfo();
+    const data = [];
 
-      const data = [];
+    // Load confirmed cases
+    const confirmedRows = await this.getConfirmedCases();
+    const confirmedHeaders = Object.keys(confirmedRows[0]);
+    const headers = [...confirmedHeaders.slice(0, 4)];
+    confirmedHeaders.slice(4).forEach(item => {
+      const idx = headers.map(i => formatDate(i)).indexOf(formatDate(item));
+      if (idx === -1) {
+        headers.push(formatDate(item));
+      } else {
+        headers[idx] = formatDate(item);
+      }
+    });
 
-      // Load confirmed sheets
-      const confirmedSheet = doc.sheetsByIndex[1];
-      const confirmedSheetRows = await confirmedSheet.getRows();
-      await confirmedSheet.loadHeaderRow();
+    // Load recovered sheet
+    const recoveredRows = await this.getRecovered();
 
-      const headers = [...confirmedSheet.headerValues.slice(0, 5)];
-      confirmedSheet.headerValues.slice(5).forEach(item => {
-        const idx = headers.map(i => formatDate(i)).indexOf(formatDate(item));
-        if (idx === -1) {
-          headers.push(formatDate(item));
-        } else {
-          headers[idx] = formatDate(item);
-        }
+    // Load recovered sheet
+    const deathRows = await this.getDeaths();
+
+    confirmedRows.forEach(row => {
+      const obj = {};
+      headers.slice(0, 4).forEach(header => {
+        obj[header] = row[header];
+      });
+      obj.dates = [];
+      headers.slice(4).forEach(header => {
+        const confirmedHeaders = Object.keys(confirmedRows[0]).filter(
+          i => formatDate(i) === header
+        );
+        const recoveredHeaders = Object.keys(recoveredRows[0]).filter(
+          i => formatDate(i) === header
+        );
+        const deathHeaders = Object.keys(deathRows[0]).filter(
+          i => formatDate(i) === header
+        );
+        obj.dates.push({
+          date: header.split(' ')[0],
+          confirmed: +row[confirmedHeaders[confirmedHeaders.length - 1]] || 0,
+          recovered:
+            +recoveredRows.find(
+              i =>
+                roundOffCoord(i.Lat) === roundOffCoord(row.Lat) &&
+                roundOffCoord(i.Long) === roundOffCoord(row.Long)
+            )[recoveredHeaders[recoveredHeaders.length - 1]] || 0,
+          death:
+            +deathRows.find(
+              i =>
+                roundOffCoord(i.Lat) === roundOffCoord(row.Lat) &&
+                roundOffCoord(i.Long) === roundOffCoord(row.Long)
+            )[deathHeaders[deathHeaders.length - 1]] || 0
+        });
       });
 
-      // Load recovered sheet
-      const recoveredSheet = doc.sheetsByIndex[2];
-      const recoveredSheetRows = await recoveredSheet.getRows();
-      await recoveredSheet.loadHeaderRow();
+      data.push(obj);
+    });
 
-      // Load death sheet
-      const deathSheet = doc.sheetsByIndex[3];
-      const deathSheetRows = await deathSheet.getRows();
-      await deathSheet.loadHeaderRow();
+    return {
+      total_confirmed: data.reduce(
+        (a, b) => a + b.dates[b.dates.length - 1].confirmed,
+        0
+      ),
+      total_recovered: data.reduce(
+        (a, b) => a + b.dates[b.dates.length - 1].recovered,
+        0
+      ),
+      total_death: data.reduce(
+        (a, b) => a + b.dates[b.dates.length - 1].death,
+        0
+      ),
+      data
+    };
+  }
 
-      confirmedSheetRows.forEach(row => {
-        const obj = {};
-        headers.slice(0, 5).forEach(header => {
-          obj[header] = row[header];
-        });
-        obj.dates = [];
-        headers.slice(5).forEach(header => {
-          const confirmedSheetHeaders = confirmedSheet.headerValues.filter(
-            i => formatDate(i) === header
-          );
-          const recoveredSheetHeaders = recoveredSheet.headerValues.filter(
-            i => formatDate(i) === header
-          );
-          const deathSheetHeaders = deathSheet.headerValues.filter(
-            i => formatDate(i) === header
-          );
+  parseCSV(url) {
+    return new Promise((resolve, reject) => {
+      const rows = [];
+      csv()
+        .fromStream(request.get(url))
+        .subscribe(
+          json => {
+            rows.push(json);
+          },
+          () => {
+            reject();
+          },
+          () => {
+            resolve(rows);
+          }
+        );
+    });
+  }
 
-          obj.dates.push({
-            date: header.split(' ')[0],
-            confirmed:
-              +row[confirmedSheetHeaders[confirmedSheetHeaders.length - 1]] ||
-              0,
-            recovered:
-              +recoveredSheetRows.find(
-                i =>
-                  i.Lat.trim() === row.Lat.trim() &&
-                  i.Long.trim() === row.Long.trim()
-              )[recoveredSheetHeaders[recoveredSheetHeaders.length - 1]] || 0,
-            death:
-              +deathSheetRows.find(
-                i =>
-                  i.Lat.trim() === row.Lat.trim() &&
-                  i.Long.trim() === row.Long.trim()
-              )[deathSheetHeaders[recoveredSheetHeaders.length - 1]] || 0
-          });
-        });
+  getConfirmedCases() {
+    return this.parseCSV(
+      `${this.timeSeriesURL}/time_series_2019-ncov-Confirmed.csv`
+    );
+  }
 
-        data.push(obj);
-      });
+  getRecovered() {
+    return this.parseCSV(
+      `${this.timeSeriesURL}/time_series_2019-ncov-Recovered.csv`
+    );
+  }
 
-      return {
-        total_confirmed: data.reduce(
-          (a, b) => a + b.dates[b.dates.length - 1].confirmed,
-          0
-        ),
-        total_recovered: data.reduce(
-          (a, b) => a + b.dates[b.dates.length - 1].recovered,
-          0
-        ),
-        total_death: data.reduce(
-          (a, b) => a + b.dates[b.dates.length - 1].death,
-          0
-        ),
-        last_update: new Date(),
-        data
-      };
-    } catch (e) {
-      throw e;
-    }
+  getDeaths() {
+    return this.parseCSV(
+      `${this.timeSeriesURL}/time_series_2019-ncov-Deaths.csv`
+    );
   }
 }
 
